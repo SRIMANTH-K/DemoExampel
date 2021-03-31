439e90767197b27d0226d26b5ff1a62ecc4a4854
srimanth <srimanthk65@gmail.com>
2021-03-31 17:06:35 +0530

    Veg and electronics tests

---
+++ src/test/java/DEMO7/Vegitablescarttest.java
@@ -0,0 +1,12 @@
+package DEMO7;
+
+import org.testng.annotations.Test;
+
+public class Vegitablescarttest {
+
+	@Test
+	public void vegtest() {
+		
+		System.out.println("this is cart testing");
+	}
+}
---
+++ src/test/java/DEMO8/Eletronicstest.java
@@ -0,0 +1,19 @@
+package DEMO8;
+
+import org.testng.annotations.Test;
+
+public class Eletronicstest {
+
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void electronicstest() {
+		
+		System.out.println("this is electronics testing");
+	}
+}
