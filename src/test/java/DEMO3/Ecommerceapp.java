d5a3a94bb32a5215e8eb53d75b811f47bb6fff5d
srimanth <srimanthk65@gmail.com>
2021-03-31 16:27:45 +0530

    EcommrceAppTest

---
+++ src/test/java/DEMO3/Ecommerceapp.java
@@ -0,0 +1,14 @@
+package DEMO3;
+
+import org.testng.annotations.Test;
+
+public class Ecommerceapp {
+
+	
+	@Test
+	public void ecommrcetest() {
+		
+		
+		System.out.println("this is test 3");
+	}
+}
