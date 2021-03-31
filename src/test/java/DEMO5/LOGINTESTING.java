ee377f8bdef00ab676423d6f5caf613f1eef5635
srimanth <srimanthk65@gmail.com>
2021-03-31 16:50:54 +0530

    NewlyAdded

---
+++ src/test/java/DEMO5/LOGINTESTING.java
@@ -0,0 +1,11 @@
+package DEMO5;
+
+public class LOGINTESTING {
+
+	
+	@Test
+	public void login() {
+		
+		System.out.println("this is login testing");
+	}
+}
---
+++ src/test/java/DEMO6/SignupTest.java
@@ -0,0 +1,12 @@
+package DEMO6;
+
+import org.testng.annotations.Test;
+
+public class SignupTest {
+
+	@Test
+	public void Signuptest() {
+		
+		System.out.println("this is Signup testing");
+	}
+}
