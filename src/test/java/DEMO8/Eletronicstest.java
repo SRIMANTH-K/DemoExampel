6965f972ddb041424931be046539cd1f3809eed2
srimanth <srimanthk65@gmail.com>
2021-05-12 17:00:57 +0530

    this is done by srimanth

--- src/test/java/DEMO8/Eletronicstest.java
+++ src/test/java/DEMO8/Eletronicstest.java
@@ -4,16 +4,14 @@ import org.testng.annotations.Test;
 
 public class Eletronicstest {
 
-	
-	
-	
-	
-	
-	
-	
 	@Test
 	public void electronicstest() {
 		
 		System.out.println("this is electronics testing");
 	}
 }
+@Test
+	public void addingkartpage() {
+		
+		System.out.println("this is addingkart testing");
+	}
